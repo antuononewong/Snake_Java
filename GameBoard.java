b9111282728fa5291e87cf0f4b31d7c252c34594
Anthony <48633290+antuononewong@users.noreply.github.com>
2020-09-08 22:21:51 -0500

    Update GameBoard.java
    - Added comments for clarity

--- GameBoard.java
+++ GameBoard.java
@@ -1,5 +1,10 @@
 import javax.swing.JFrame;
 
+/* Class that draws play space, player, and eggs into pre-defined window. 
+ * Sets up game defaults like player, board size, and scheduler.
+ * Sets up defaults of game window.
+ */
+
 public class GameBoard {
 	JFrame frame = new JFrame("Snake");
 	int height = 800;
@@ -9,6 +14,7 @@ public class GameBoard {
 		Egg egg = new Egg();
 		Player player = new Player(egg);
 		Board board = new Board(player, egg);
+		
 		SnakeScheduler scheduler = new SnakeScheduler(board, player, egg);
 		player.setScheduler(scheduler);
 		scheduler.start();
